d2f62ca7bbb6a09f4d718d3531f517737b6bfb58
Solovejko <solovejko00@mail.ru>
2020-03-13 22:27:32 +0300

    up

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -1,23 +1,126 @@
 package ru.itmo.java;
 
-import java.util.Map;
+import java.lang.annotation.ElementType;
+import java.util.Arrays;
+import java.util.EventListener;
 
 public class HashTable {
+    private final static int INITIAL_CAPACITY = 10;
+    private double loadFactor;
+    private int Size;
+    private Entry[] Elements;
 
-    Object put(Object key, Object value) {
-        throw new UnsupportedOperationException();
+    public HashTable() {
+        this(INITIAL_CAPACITY);
     }
 
-    Object get(Object key) {
-        throw new UnsupportedOperationException();
+    public HashTable(int size) {
+        this(size, 0.5);
     }
 
-    Object remove(Object key) {
-        throw new UnsupportedOperationException();
+    public HashTable(int size, double loadFactor) {
+        Elements = new Entry[size];
+        this.loadFactor = loadFactor;
     }
 
-    int size() {
-        throw new UnsupportedOperationException();
+    public Object put(Object key, Object value) {
+        int hs = Math.abs(key.hashCode() % Elements.length);
+
+        Object OutValue = null;
+
+        for (int i = 0; i < Elements.length && OutValue == null; i++, hs = (hs + 1) % Elements.length) {
+            if ((Elements[hs] != null) && (Elements[hs].key.equals(key))) {
+                OutValue = Elements[hs].value;
+                Elements[hs++] = new Entry(key, value);
+            }
+        }
+
+        if (OutValue != null)
+            return OutValue;
+
+        while (Elements[hs] != null) {
+            hs = (hs + 1) % Elements.length;
+        }
+
+        Elements[hs] = new Entry(key, value);
+        Size++;
+
+        if ((double) Size / Elements.length > loadFactor)
+            IncreaseCapacity();
+
+        return null;
     }
 
+    public Object get(Object key) {
+        Object OutValue = null;
+
+        for (int i = 0, hs = Math.abs(key.hashCode() % Elements.length); i < Elements.length && OutValue == null; i++, hs = (hs + 1) % Elements.length) {
+            if ((Elements[hs] != null) && (Elements[hs].key.equals(key))) {
+                OutValue = Elements[hs].value;
+            }
+        }
+
+        return OutValue;
+    }
+
+    public Object remove(Object key) {
+        Object OutValue = null;
+
+        for (int i = 0, hs = Math.abs(key.hashCode() % Elements.length); i < Elements.length && OutValue == null; i++, hs = (hs + 1) % Elements.length) {
+            if ((Elements[hs] != null) && (Elements[hs].key.equals(key))) {
+                OutValue = Elements[hs].value;
+                Elements[hs] = null;
+                Size--;
+            }
+        }
+
+        return OutValue;
+    }
+
+    public int size() {
+        return Size;
+    }
+
+
+    private void IncreaseCapacity() {
+        Entry[] newElements = new Entry[Elements.length * 2];
+
+        for (int i = 0; i < Elements.length; i++) {
+            if (Elements[i] == null)
+                continue;
+
+            int hs = Math.abs(Elements[i].key.hashCode() % newElements.length);
+
+            while (newElements[hs] != null)
+                hs = (hs + 1) % newElements.length;
+
+            newElements[hs] = Elements[i];
+        }
+
+        Elements = newElements;
+    }
+
+
+    @Override
+    public String toString() {
+
+        Object[] m = new Object[Elements.length];
+
+        for (int i = 0; i < Elements.length; i++)
+            if (Elements[i] != null)
+                m[i] = Elements[i].value;
+
+        return Arrays.toString(m);
+    }
+
+
+    private class Entry {
+        Object key, value;
+
+        public Entry(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
+
